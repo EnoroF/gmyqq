78ddb482c79586525d4657f229a2570f8b23d016
amoblin <amoblin@4396dc02-308c-11de-b310-9106b1f7c239>
2009-04-28 02:30:28 +0000

    1.add gmyqq.glade and test.py

---
+++ trunk/data/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import gtk
+import gtk.glade
+
+class MainWindow():
+    def __init__(self):
+        self.gladefile="gmyqq.glade"
+        self.xml=gtk.glade.XML(self.gladefile)
+        self.window = self.xml.get_widget("window_main")
+
+        self.window.set_title("gmyqq")
+        self.window.set_default_size(800, 600)
+        self.window.connect('destroy', gtk.main_quit)      
+        self.window.show_all();
+        
+def main():
+    win = MainWindow();
+    gtk.main()
+
+
+if __name__ == '__main__':
+    main()
